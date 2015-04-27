d8d1e4a942a944c5555b0de8ccda90bcf59f86a7
adry.indah@gmail.com <adry.indah@gmail.com>
2015-04-27 09:32:55 +0800

    adri

---
+++ Assets/playerjump.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerjump : MonoBehaviour {
+	public float longjump,walk;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+						GetComponent<Rigidbody>().velocity = Vector3.zero;
+						GetComponent<Rigidbody>().AddForce (Vector3.up * longjump);
+				}
+		transform.position +=(Vector3.right/20);
+	}
+}
